f560fea132fbbce90296b3dcb8cf88ab6b7f3a83
Jody Sergison <jsergison@airmedia-0ea030.tahoedonner.com>
2016-04-18 10:59:27 -0700

    updates 4-19

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,74 +1,97 @@
 angular.module('app.controllers', [])
-  
+
 .controller('homeCtrl', function($scope) {
 
 })
-   
+
 .controller('casaCtrl', function($scope) {
 
 })
-   
+
 .controller('winesCtrl', function($scope) {
 
 })
-   
+
 .controller('vinhosCtrl', function($scope) {
 
 })
-   
+
 .controller('historyCtrl', function($scope) {
 
 })
-   
+
 .controller('histRiaCtrl', function($scope) {
 
 })
-   
+
 .controller('vineyardsCtrl', function($scope) {
 
 })
-   
+
 .controller('vineyardsPortugueseCtrl', function($scope) {
 
 })
-   
+
 .controller('regionsCtrl', function($scope) {
 
 })
-   
+
 .controller('regiEsCtrl', function($scope) {
 
 })
-   
+
 .controller('posCtrl', function($scope) {
 
 })
-   
+
 .controller('piontDeVendaCtrl', function($scope) {
 
 })
-   
+
 .controller('whiteVaretalsCtrl', function($scope) {
 
 })
-   
+
 .controller('brancoVaretalsCtrl', function($scope) {
 
 })
-   
+
 .controller('redVaretalsRosCtrl', function($scope) {
 
 })
-   
+
 .controller('vermelhoVaretalsRosaCtrl', function($scope) {
 
 })
-   
+
 .controller('redVaretalsCtrl', function($scope) {
 
 })
-   
+
 .controller('vermelhoVaretalsCtrl', function($scope) {
 
 })
-    
+
+.controller('portalCtrl', function($scope) {
+
+})
+
+.controller('lagoCtrl', function($scope) {
+
+})
+
+.controller('quintaCtrl', function($scope) {
+
+})
+
+.controller('ourVineyardsCtrl', function($scope) {
+
+})
+
+.controller('usaCtrl', function($scope) {
+
+})
+
+.controller('prtCtrl', function($scope) {
+
+})
